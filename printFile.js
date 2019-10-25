de7dd6998453174d6488e55198e40c761ce3d765
David Krajewski <davidkrajewski@yahoo.com>
2019-10-24 21:02:59 -0400

    Added new formatting for logging

--- printFile.js
+++ printFile.js
@@ -1,5 +1,9 @@
 let MESSAGE = "TEST_MESSAGE";
 console.log(MESSAGE);
 
-let TABLE = [1, 2, 3]
+let TABLE = [1, 2, 3];
 console.table(TABLE);
+
+let DEBUG_MESSAGE = "Error on line 7.";
+let DEBUG_VAL = 4;
+console.debug(DEBUG_MESSAGE, DEBUG_VAL);
